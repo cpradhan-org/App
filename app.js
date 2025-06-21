d7b2ac96a70bf6f985e8830e600ecdf0bbff5874
chinmaya <chinmaya@gmail.com>
2025-06-21 10:45:35 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const fs = require('fs')
 const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+const serverless = require('serverless-http')
 
 
 app.use(bodyParser.json());
@@ -56,7 +58,17 @@ app.get('/',   async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
-
+app.get('/api-docs', (req, res) => {
+    fs.readFile('oas.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        res.status(500).send('Error reading file');
+      } else {
+        res.json(JSON.parse(data));
+      }
+    });
+  });
+  
 app.get('/os',   function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
@@ -79,9 +91,7 @@ app.get('/ready',   function(req, res) {
     });
 })
 
-app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
-
+app.listen(3000, () => { console.log("Server successfully running on port - " +3000); })
 module.exports = app;
+
+//module.exports.handler = serverless(app)
